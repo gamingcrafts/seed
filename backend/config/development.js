9ac77440db52eb3524881d06dc0d3b81b68d6efb
Santhosh <santhoshmani30@gmail.com>
2019-01-29 19:01:51 +0200

    backend crud

--- backend/config/development.js
+++ backend/config/development.js
@@ -5,13 +5,13 @@ module.exports = {
     port: 8000
   },
   elasticsearch: {
-    host: "https://18f5c48dd62340748334f3bc4d373a0c.ap-southeast-1.aws.found.io:9243",
+    host: "",
     log: "trace",
     keepAlive: true,
     minSockets: 5,
     maxSockets: 20,
     requestTimeout: 120000,
-    httpAuth: 'elastic:i8GUm9s4Wph3o7GUOi4bf7GU'
+    httpAuth: ''
   },
   indexes: {
     sample: 'sample__sample'
