92a7d750fdd5a9b4c5d3ccdc019d57b4c91c23ff
Srinivas <srinivas@marvellabs.in>
2019-02-07 20:32:57 +0530

    Create Custom Mappings Index

---
+++ backend/mappings/custom_mappings.js
@@ -0,0 +1,35 @@
+const custom_mappings={
+    "indexName":{
+        "type":"text"
+    },
+    "properties":{
+        fieldName:{
+            type:"text"
+        },
+        fieldDefinition:{
+            type:"text"
+        },
+        label:{
+            type:"text"
+        },
+        sortable:{
+            type:"boolean"
+        },
+        selected:{
+            type:"boolean"
+        },
+        dateColumn:{
+            type:"boolean"
+        },
+        currencyColumn:{
+            type:"boolean"
+        },
+        format:{
+            type:"text"
+        }
+    }
+}
+
+module.exports = {
+    custom_mappings
+  }
--- backend/prerun.js
+++ backend/prerun.js
@@ -1,6 +1,7 @@
 const ESClient = require('./server/esclient');
 
 const kpis = require('./mappings/player_daily_kpis');
+const custom_mappings = require('./mappings/custom_mappings');
 
 const run = async () => {
   let c = new ESClient();
@@ -8,6 +9,8 @@ const run = async () => {
   let res = await c.client().indices.exists({
     index: 'kpis'
   })
+
+  
 
   if(!res) {
     new ESClient().client().indices.create({
@@ -17,8 +20,23 @@ const run = async () => {
           ...kpis
         }
       }
+    });
+
+    new ESClient().client().indices.create({
+      index: 'custom_mappings',
+      body: {
+        mappings: {
+          ...custom_mappings
+        }
+      }
     })
   }
+
+  c.client().indices.get({index:"*"},(err,res)=>{
+    console.log("-------------------------------")
+    console.log(res);
+    console.log("-------------------------------")
+  })
 }
  
 module.exports = {
