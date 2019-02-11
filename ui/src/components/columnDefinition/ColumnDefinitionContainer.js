c75b690e8b6bb98f1f28aa344eb9229cd112d7eb
Santhosh <santhoshmani30@gmail.com>
2019-02-11 09:47:49 +0200

    changes

--- ui/src/components/columnDefinition/ColumnDefinitionContainer.js
+++ ui/src/components/columnDefinition/ColumnDefinitionContainer.js
@@ -4,68 +4,64 @@ import {
   getIndices
 } from '../store/actions/column-definition-actions';
 import {
-    EuiPage,
-    EuiPageBody,
-    EuiPageContent,EuiSpacer, EuiBasicTable,
-    EuiButton,
-    EuiComboBox
-  } from '@elastic/eui';
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent, EuiSpacer, EuiBasicTable,
+  EuiButton,
+  EuiComboBox
+} from '@elastic/eui';
 
-  class ColumnDefinitionContainer extends React.Component{
+class ColumnDefinitionContainer extends React.Component {
 
-    constructor(props){
-      super(props);
-      this.indices=[];
-      this.state={
-        selectedOption:[]
-      };
-
-      
-    }
-    componentDidMount(){
-      this.props.getIndices();
-    }
-    onChange = (selectedOptions) => {
-      this.setState({
-        selectedOption:selectedOptions[0].label,
-      });
+  constructor(props) {
+    super(props);
+    this.indices = [];
+    this.state = {
+      selectedOption: []
     };
-    onCreateOption = (searchValue, flattenedOptions) => {
-      // this.setState(prevState => ({
-      //   selectedOptions: [].concat(searchValue),
-      // }));
-    }
+  }
+  componentDidMount() {
+    this.props.getIndices();
+  }
 
-    render(){
-      let {indices} = this.props.columnDefinitionReducer;
-      if(this.indices.length===0){
-        Object.keys(indices).forEach((index)=>{
-          if(index!=='custom_mapping'){
-            this.indices.push({label:index});
-          }
-        });
-      }
-      if(indices===undefined)
-      {
-        return null;
-      }
-      else{
-    return (
+  onChange = (selectedOption) => {
+    this.setState({ selectedOption });
+  };
+  onCreateOption = (searchValue, flattenedOptions) => {
+    // this.setState(prevState => ({
+    //   selectedOptions: [].concat(searchValue),
+    // }));
+  }
+
+  render() {
+    let { indices } = this.props.columnDefinitionReducer;
+    if (this.indices.length === 0) {
+      Object.keys(indices).forEach((index) => {
+        if (index !== 'custom_mapping') {
+          this.indices.push({ label: index });
+        }
+      });
+    }
+    if (indices === undefined) {
+      return null;
+    }
+    else {
+      return (
         <EuiComboBox
           placeholder="Select an Index"
           options={this.indices}
           onChange={this.onChange}
-        
+          selectedOptions={this.state.selectedOption}
           singleSelection={{ asPlainText: true }}
         />
       );
-      }
     }
-
   }
 
-  const mapStateToProps = ({ columnDefinitionReducer }) => {
-    return { columnDefinitionReducer }
-  }
-  
-  export default connect(mapStateToProps, {getIndices})(ColumnDefinitionContainer)
+}
+
+const mapStateToProps = ({ columnDefinitionReducer }) => {
+  return { columnDefinitionReducer }
+}
+
+export default connect(mapStateToProps, { getIndices })(ColumnDefinitionContainer)
