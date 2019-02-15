c9a1fb4649dd1aab9fcd5bdcd06ab6a9ee74f394
Santhosh <santhoshmani30@gmail.com>
2019-02-15 11:21:50 +0200

    updated

--- ui/src/components/columnDefinition/ColumnDefinitionContainer.js
+++ ui/src/components/columnDefinition/ColumnDefinitionContainer.js
@@ -1,8 +1,8 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ReactJson from 'react-json-view';
 import {
-  getIndices,getIndexCustomMapping
+  getIndices, getIndexCustomMapping
 } from '../store/actions/column-definition-actions';
 import {
   EuiPage,
@@ -14,7 +14,7 @@ import {
   EuiPageHeader,
   EuiPageHeaderSection,
   EuiTitle, EuiSpacer, EuiBasicTable,
-  EuiButton,EuiCheckbox,EuiFieldText,
+  EuiButton, EuiCheckbox, EuiFieldText,
   EuiComboBox,
   EuiFlexGroup,
   EuiFlexItem
@@ -24,264 +24,264 @@ class ColumnDefinitionContainer extends Component {
 
   constructor(props) {
     super(props);
-    
-    this.indices=[];
-    
-    this.mappings=[];
     this.state = {
+      columnDefinitions: {
+
+      },
       selectedOption: [],
-      mappings:[],
+      mappings: [],
       pageIndex: 0,
-      pageSize: 6,
-      showPerPageOptions: true
+      pageSize: 20
     }
   }
+
   componentDidMount() {
     this.props.getIndices();
-    
   }
 
-  onCheckBoxChange = (e) => {
-    console.log(e);
+  onCheckBoxChange = (e, item, type) => {
+    const { columnDefinitions } = this.state;
+
+    this.setState({
+      columnDefinitions: {
+        ...columnDefinitions,
+        [item]: {
+          ...columnDefinitions[item],
+          [type]: e.target.checked
+        }
+      }
+    });
   };
-  
+
+  onTextBoxChange = (e, item, type) => {
+    const { columnDefinitions } = this.state;
+
+    this.setState({
+      columnDefinitions: {
+        ...columnDefinitions,
+        [item]: {
+          ...columnDefinitions[item],
+          [type]: e.target.value
+        }
+      }
+    });
+  }
+
 
   onChange = (selectedOption) => {
-   console.log(selectedOption)
-
-    if(selectedOption.length===0){
+    if (selectedOption.length === 0) {
       this.setState({
-        selectedOption:[],
-        mappings:[]
+        selectedOption: [],
+        mappings: []
       })
-      
     }
-    else{
+    else {
       this.setState({ selectedOption });
-    this.props.getIndexCustomMapping(selectedOption[0].label);
+      this.props.getIndexCustomMapping(selectedOption[0].label);
     }
   };
 
-    onSearchChange=(arg)=>{
-      console.log(arg);
-    };
+  onSearchChange = (arg) => {
+    console.log(arg);
+  };
 
-    onTableChange = ({ page = {} }) => {
-      
-      const {
-        index: pageIndex,
-        size: pageSize,
-      } = page;
-  
-      this.setState({
-        pageIndex,
-        pageSize,
-      });
-    };
-  
+  onTableChange = ({ page = {} }) => {
+    const {
+      index: pageIndex,
+      size: pageSize,
+    } = page;
+
+    this.setState({
+      pageIndex,
+      pageSize,
+    });
+  };
+
   render() {
-    let { indices,mappings } = this.props.columnDefinitionReducer;
-    this.indices=indices;
-    
-    this.mappings=mappings;
-   
-    if (this.indices === undefined) {
-      return null;
-    }
-    else {
-      const {
-        pageIndex,
-        pageSize,
-        showPerPageOptions
-      } = this.state;
-      const pagination = {
-        pageIndex ,
-        pageSize ,
-        totalItemCount:20,
-        pageSizeOptions: [10, 20, 30],
-        showPerPageOptions
-      };
-      console.log("------Pagination----------")
-      console.log(pagination);
-      const columns = [{
-        field: 'fieldName',
-        name: 'Column Name',
-        sortable: true,
-        width:'15%',
-        hideForMobile: false,
-        'data-test-subj': 'columnNameCell',
-      },
-      {
-        field: 'fieldDefinition',
-        name: 'Column Type',
-        sortable: true,
-        width:'25%',
-        hideForMobile: false,
-        render: c => (<ReactJson src={c} name={false} collapsed={true}/>),
-        'data-test-subj': 'columnTypeCell',
-      },
-      {
-        field: 'label',
-        name: 'Label',
-        sortable: false,
-        width:'20%',
-        hideForMobile: true,
-        render:l=>(<EuiFieldText
-          placeholder=""
+    let { indices, mappings } = this.props.columnDefinitionReducer;
+    const { columnDefinitions } = this.state
+
+    const {
+      pageIndex,
+      pageSize,
+
+    } = this.state;
+    const pagination = {
+      pageIndex,
+      pageSize,
+      totalItemCount: 20,
+      pageSizeOptions: [10, 20, 30],
+
+    };
+    const columns = [{
+      field: 'fieldName',
+      name: 'Column Name',
+      sortable: true,
+      width: '15%',
+      hideForMobile: false,
+      'data-test-subj': 'columnNameCell',
+    },
+    {
+      field: 'fieldDefinition',
+      name: 'Column Type',
+      sortable: true,
+      width: '25%',
+      hideForMobile: false,
+      render: c => (<ReactJson src={c} name={false} collapsed={true} />),
+      'data-test-subj': 'columnTypeCell',
+    },
+    {
+      field: 'label',
+      name: 'Label',
+      sortable: false,
+      width: '20%',
+      hideForMobile: true,
+      render: (value, item) => (
+        <EuiFieldText
+          placeholder="label to represent in table"
+          value={columnDefinitions[item.fieldName] ? columnDefinitions[item.fieldName].label : ''}
+          onChange={(e) => this.onTextBoxChange(e, item.fieldName, 'label')}
           aria-label="Use aria labels when no actual label is in use"
-        />),
-        'data-test-subj': 'labelCell',
+        />
+      ),
+      'data-test-subj': 'labelCell',
+    },
+    {
+      field: 'selected',
+      name: 'Selected',
+      sortable: false,
+      width: '10%',
+      hideForMobile: true,
+      render: (value, item) => {
+        return (
+          <EuiCheckbox
+            id={item.fieldName}
+            checked={columnDefinitions[item.fieldName] ? columnDefinitions[item.fieldName].selected : false}
+            onChange={(e) => this.onCheckBoxChange(e, item.fieldName, 'selected')} />
+        )
       },
-      {
-        field: 'seleted',
-        name: 'Selected',
-        sortable: false,
-        width:'10%',
-        hideForMobile: true,
-        render:(isSelected,item)=>{
-         return( <EuiCheckbox
+      'data-test-subj': 'selectedCell',
+    },
+    {
+      field: 'sortable',
+      name: 'Sortable',
+      sortable: false,
+      width: '10%',
+      hideForMobile: true,
+      render: (isSortable, item) => (
+        <EuiCheckbox
           id={item.fieldName}
-          
-          checked={isSelected}
-          onChange={()=>this.onCheckBoxChange(item)}
-          
-        />)
-        },
-        'data-test-subj': 'selectedCell',
-      },
-      {
-        field: 'sortable',
-        name: 'Sortable',
-        sortable: false,
-        width:'10%',
-        hideForMobile: true,
-        render:(isSortable,item)=>{
-         return( <EuiCheckbox
-          id={item.fieldName}
-          
           checked={isSortable}
           onChange={this.onCheckBoxChange}
-          
-        />)
-        },
-        'data-test-subj': 'sortableCell',
-      },
-      {
-        field: 'dateColumn',
-        name: 'Date Column',
-        sortable: false,
-        width:'10%',
-        hideForMobile: true,
-        render:(isDateColumn,item)=>{
-         return( <EuiCheckbox
+        />
+      ),
+      'data-test-subj': 'sortableCell',
+    },
+    {
+      field: 'dateColumn',
+      name: 'Date Column',
+      sortable: false,
+      width: '10%',
+      hideForMobile: true,
+      render: (isDateColumn, item) => {
+        return (<EuiCheckbox
           id={item.fieldName}
-          
+
           checked={isDateColumn}
           onChange={this.onCheckBoxChange}
-          
+
         />)
-        },
-        'data-test-subj': 'dataColumnCell',
       },
-      {
-        field: 'currencyColumn',
-        name: 'Currency Column',
-        sortable: false,
-        width:'10%',
-        hideForMobile: true,
-        render:(isCurrencyColumn,item)=>{
-         return( <EuiCheckbox
+      'data-test-subj': 'dataColumnCell',
+    },
+    {
+      field: 'currencyColumn',
+      name: 'Currency Column',
+      sortable: false,
+      width: '10%',
+      hideForMobile: true,
+      render: (isCurrencyColumn, item) => {
+        return (<EuiCheckbox
           id={item.fieldName}
-          
+
           checked={isCurrencyColumn}
           onChange={this.onCheckBoxChange}
-          
+
         />)
-        },
-        'data-test-subj': 'currencyColumnCell',
       },
-      {
-        field: 'format',
-        name: 'Format',
-        sortable: false,
-        width:'20%',
-        hideForMobile: true,
-        render:f=>(<EuiFieldText
-          placeholder=""
-          aria-label="Use aria labels when no actual label is in use"
-        />),
-        'data-test-subj': 'formatCell',
-      }
-    ] 
-      return (
-        <EuiPage>
-    <EuiPageBody>
-      <EuiPageHeader>
-        <EuiPageHeaderSection>
-          <EuiTitle size="l">
-            <h1>Indices Column Definition</h1>
-          </EuiTitle>
-        </EuiPageHeaderSection>
-      </EuiPageHeader>
-      <EuiPageContent>
-        <EuiPageContentHeader>
-          <EuiPageContentHeaderSection>
-            <EuiTitle>
-              <h2>Index :</h2>
-            </EuiTitle>
-          </EuiPageContentHeaderSection>
-        </EuiPageContentHeader>
-        <EuiPageContentBody>
-        <EuiFlexGroup>
-          <EuiFlexItem grow={10}>
-        <EuiComboBox
-          placeholder="Select an Index"
-          options={this.indices}
-          onChange={this.onChange}
-          onSearchChange={this.onSearchChange}
-          selectedOptions={this.state.selectedOption}
-          singleSelection={{ asPlainText: true }}
-        />
-        </EuiFlexItem>
-        <EuiFlexItem>
-        <EuiButton
-          fill
-          color="secondary"
-        >
-          Save
-        </EuiButton>
-        </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiSpacer/>
-        
-
-    
-    <EuiFlexGroup>
-         <EuiFlexItem>
-        <EuiBasicTable
-      items={this.mappings}
-      columns={columns}
-      pagination = {pagination}
-      onChange={this.onTableChange}
-    />
-    </EuiFlexItem>
-    </EuiFlexGroup>
-        </EuiPageContentBody>
-      </EuiPageContent>
-    </EuiPageBody>
-  </EuiPage>
-        
-    
-
-
-      );
+      'data-test-subj': 'currencyColumnCell',
+    },
+    {
+      field: 'format',
+      name: 'Format',
+      sortable: false,
+      width: '20%',
+      hideForMobile: true,
+      render: f => (<EuiFieldText
+        placeholder=""
+        aria-label="Use aria labels when no actual label is in use"
+      />),
+      'data-test-subj': 'formatCell',
     }
+    ]
+    return (
+      <EuiPage>
+        <EuiPageBody>
+          <EuiPageHeader>
+            <EuiPageHeaderSection>
+              <EuiTitle size="l">
+                <h1>Indices Column Definition</h1>
+              </EuiTitle>
+            </EuiPageHeaderSection>
+          </EuiPageHeader>
+          <EuiPageContent>
+            <EuiPageContentHeader>
+              <EuiPageContentHeaderSection>
+                <EuiTitle>
+                  <h2>Index :</h2>
+                </EuiTitle>
+              </EuiPageContentHeaderSection>
+            </EuiPageContentHeader>
+            <EuiPageContentBody>
+              <EuiFlexGroup>
+                <EuiFlexItem grow={10}>
+                  <EuiComboBox
+                    placeholder="Select an Index"
+                    options={indices}
+                    onChange={this.onChange}
+                    onSearchChange={this.onSearchChange}
+                    selectedOptions={this.state.selectedOption}
+                    singleSelection={{ asPlainText: true }}
+                  />
+                </EuiFlexItem>
+                <EuiFlexItem>
+                  <EuiButton
+                    fill
+                    onClick={() => console.log(this.state.columnDefinitions)}
+                    color="secondary">Save</EuiButton>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+              <EuiSpacer />
+              <EuiFlexGroup>
+                <EuiFlexItem>
+                  <EuiBasicTable
+                    items={mappings}
+                    columns={columns}
+                    pagination={pagination}
+                    onChange={this.onTableChange}
+                  />
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiPageContentBody>
+          </EuiPageContent>
+        </EuiPageBody>
+      </EuiPage>
+    );
   }
-
 }
 
 const mapStateToProps = ({ columnDefinitionReducer }) => {
   return { columnDefinitionReducer }
 }
 
-export default connect(mapStateToProps, { getIndices,getIndexCustomMapping })(ColumnDefinitionContainer)
+export default connect(mapStateToProps, { getIndices, getIndexCustomMapping })(ColumnDefinitionContainer)
