71b9591d538aa0d1dedbfad2fc70a1ac1ec4f716
Srinivas <srinivas@marvellabs.in>
2019-02-26 21:28:38 +0530

    Rule Engine Operators UI Cleaned.

--- ui/src/components/rule-engine/RuleEngineOperatorsForm.js
+++ ui/src/components/rule-engine/RuleEngineOperatorsForm.js
@@ -10,7 +10,10 @@ import {
     EuiButton,
     EuiFieldText,
     EuiFieldNumber,
-    EuiTextArea
+    // EuiTextArea,
+    EuiCodeEditor,
+    EuiFormRow,
+    EuiSpacer
   
   } from '@elastic/eui';
  import {
@@ -18,15 +21,25 @@ import {
     updateOperatorsTextBox,updateOperatorsNumberBox,
     updateOperators
   } from '../store/actions/rule-engine-actions'
+
+  import 'brace/theme/github';
+  import 'brace/mode/javascript';
+  import 'brace/snippets/javascript';
+  import 'brace/ext/language_tools';
 class RuleEngineOperatorsForm extends Component {
 
   onTextChange =(e,type)=>{
+    console.log(e);
     let textBoxData = {type:type,value:e.target.value};
     this.props.updateOperatorsTextBox(textBoxData);
   }
   onNumberChange=(e,type)=>{
     let numberBoxData = {type:type,value:e.target.value};
     this.props.updateOperatorsNumberBox(numberBoxData);
+  }
+  onCodeChange=(value)=>{
+    let textBoxData = {type:'formatOp',value:value};
+    this.props.updateOperatorsTextBox(textBoxData);
   }
   updateOperators=()=>{
     let {operators,operatorsState}=this.props.RuleEngineReducer;
@@ -49,38 +62,82 @@ render(){
       <EuiPageContent>
       <EuiPageContentBody>
       <EuiFlexGroup>
-        <EuiFlexItem >
+      <EuiFlexItem >
+     
+        
+        <EuiFormRow
+          label="Name"
+        >
         <EuiFieldText
           placeholder="Name"
           value={selectedOperator.name}
           onChange={(e)=>{this.onTextChange(e,'name')}}
         />
+        </EuiFormRow>
+        <EuiFormRow
+          label="Label"
+        >
         <EuiFieldText
           placeholder="Label"
           value={selectedOperator.label}
           onChange={(e)=>{this.onTextChange(e,'label')}}
         />
+        </EuiFormRow>
+        <EuiFormRow
+          label="Label For Format"
+        >
         <EuiFieldText
           placeholder="Label For Format"
           value={selectedOperator.labelForFormat}
           onChange={(e)=>{this.onTextChange(e,'labelForFormat')}}
         />
+        </EuiFormRow>
+        <EuiFormRow
+          label="Cardinality"
+        >
         <EuiFieldNumber
           placeholder="Cardinality"
           value={selectedOperator.cardinality}
             onChange={(e)=>this.onNumberChange(e,'cardinality')}
         />
+        </EuiFormRow>
+        </EuiFlexItem>
+        <EuiFlexItem >
+        <EuiFormRow
+          label="Reversed Operator"
+        >
         <EuiFieldText
           placeholder="Reversed Operator"
           value={selectedOperator.reversedOp}
           onChange={(e)=>{this.onTextChange(e,'reversedOp')}}
         />
-        <EuiTextArea
+        </EuiFormRow>
+        <EuiFormRow
+          label="FormatOp"
+        >
+        {/* <EuiTextArea
           placeholder="FormatOp"
           onChange={(e)=>{this.onTextChange(e,'formatOp')}}
           value={selectedOperator.formatOp}
         
-        />
+        /> */}
+
+        <EuiCodeEditor
+        mode="javascript"
+        theme="github"
+        width="100%"
+        value={selectedOperator.formatOp}
+        onChange={this.onCodeChange}
+        setOptions={{
+          fontSize: '14px',
+          enableBasicAutocompletion: true,
+          enableSnippets: true,
+          enableLiveAutocompletion: true,
+        }}
+        onBlur={() => { console.log('blur'); }} // eslint-disable-line no-console
+        aria-label="Code Editor"
+      />
+        </EuiFormRow>
         </EuiFlexItem>
         <EuiFlexItem >
         <EuiButton
@@ -88,6 +145,7 @@ render(){
         >
           Save
         </EuiButton>
+        <EuiSpacer size="xl" />
         <EuiButton color="warning"
           onClick={() => this.props.toogleRuleEngineOperatorList()}
         >
